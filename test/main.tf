3a11e923c8e57cbb8adcba718722411086a74983
tejas-shettyy <142570994+tejas-shettyy@users.noreply.github.com>
2024-07-01 16:48:44 +0530

    main.tf

--- test/main.tf
+++ test/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.15.00"
-    }
-  }
-  backend "azurerm" {
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "azure_rg" {
   name     =  var.rgname
   location =  var.location
