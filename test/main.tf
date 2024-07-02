057b162e9e962c5ca9c69141bf7b77aaef1efeda
tejas-shettyy <142570994+tejas-shettyy@users.noreply.github.com>
2024-07-02 11:49:06 +0530

    main.tf

--- test/main.tf
+++ test/main.tf
@@ -32,25 +32,3 @@ resource "azurerm_subnet" "subnet" {
   name                 = each.value["name"]
   address_prefixes     = each.value["address_prefixes"]
 }
-
-resource "azurerm_public_ip" "bastion_pubip" {
-  name                = "${var.bastionhost_name}PubIP"
-  location            = azurerm_resource_group.vnet_rg.location
-  resource_group_name = azurerm_resource_group.vnet_rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
-
-resource "azurerm_bastion_host" "bastion" {
-  name                = var.bastionhost_name
-  location            = azurerm_resource_group.vnet_rg.location
-  resource_group_name = azurerm_resource_group.vnet_rg.name
-  tags                = var.tags
-
-  ip_configuration {
-    name                 = "bastion_config"
-    subnet_id            = azurerm_subnet.subnet["bastion_subnet"].id
-    public_ip_address_id = azurerm_public_ip.bastion_pubip.id
-  }
-}
